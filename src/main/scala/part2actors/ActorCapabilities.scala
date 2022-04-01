69ff3fdd7f0f95a07d9f5a6303f82aa3983575a1
Jiacheng Li <cheng2029@foxmail.com>
2022-04-02 01:23:29 +0800

    Add more exercises to ActorCapabilities - April 2nd

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,6 +1,8 @@
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import part2actors.ActorCapabilities.BankAccount.Deposit
+import part2actors.ActorCapabilities.Person.LiveTheLife
 
 object ActorCapabilities extends App {
 
@@ -47,7 +49,7 @@ object ActorCapabilities extends App {
   alice ! SayHiTo(bob)
 
   // 4 - dead letters
-  alice ! "Hi" // replay to "me" but "me" is null
+  // alice ! "Hi" // replay to "me" but "me" is null
   // Output:
   // [INFO] [akkaDeadLetter][03/31/2022 22:50:54.291] [actorCapabilitiesDemo-akka.actor.default-dispatcher-6]
   // [akka://actorCapabilitiesDemo/deadLetters] Message [java.lang.String]
@@ -64,4 +66,116 @@ object ActorCapabilities extends App {
 
   case class WirelessPhoneMessage(content: String, rec: ActorRef)
   alice ! WirelessPhoneMessage("Hi", bob) // no sender
+
+  /**
+   * Exercises
+   *
+   * 1. a Counter actor
+   * - Increment
+   * - Decrement
+   * - Print internal count
+   *
+   * 2. a Bank account as an actor
+   * receives
+   * - Deposit an amount
+   * - Withdraw an amount
+   * - Statement
+   * replies
+   * - Success
+   * - Failure
+   * take cares of edge cases
+   *
+   * interact with some other kind of actor
+   */
+
+  // DOMAIN of the counter, in practice
+  object Counter {
+    case object Increment
+    case object Decrement
+    case object Print
+  }
+  class Counter extends Actor {
+    import Counter._
+
+    var count: Int = 0
+
+    override def receive: Receive = {
+      case Increment => count += 1
+      case Decrement => count -= 1
+      case Print => println(s"[counter actor] My current count is $count")
+      case _ =>
+    }
+  }
+
+  import Counter._
+  val counter = system.actorOf(Props[Counter], "myCounter")
+  (1 to 5).foreach(_ => counter ! Increment)
+  (1 to 3).foreach(_ => counter ! Decrement)
+  counter ! Print
+
+  // DOMAIN of a bank account
+  object BankAccount {
+    case class Deposit(amount: Int)
+    case class Withdraw(amount: Int)
+    case object Statement
+
+    case class TransactionSuccess(message: String)
+    case class TransactionFailure(reason: String)
+  }
+  class BankAccount extends Actor {
+    import BankAccount._
+
+    var funds = 0
+
+    override def receive: Receive = {
+      case Deposit(amount) =>
+        if (amount < 0) sender() ! TransactionFailure("[bank account] Invalid deposit amount!")
+        else {
+          funds += amount
+          sender() ! TransactionSuccess(s"[bank account] Successfully deposited $amount")
+        }
+      case Withdraw(amount) =>
+        if (amount < 0) sender() ! TransactionFailure("[bank account] Invalid deposit amount!")
+        else if (amount > funds) sender() ! TransactionFailure("[bank account] Insufficient funds!")
+        else {
+          funds -= amount
+          sender() ! TransactionSuccess(s"[bank account] Successfully withdrew $amount")
+        }
+      case Statement => sender() ! s"Your balance is $funds"
+    }
+  }
+
+  object Person {
+    case class LiveTheLife(account: ActorRef)
+  }
+  class Person extends Actor {
+    import Person._
+    import BankAccount._
+
+    override def receive: Receive = {
+      case LiveTheLife(account) =>
+        account ! Deposit(10000)
+        account ! Withdraw(90000)
+        account ! Withdraw(500)
+        account ! Statement
+      case TransactionSuccess(message) => println(s"$message")
+      case TransactionFailure(reason) => println(s"$reason")
+    }
+  }
+
+  val account = system.actorOf(Props[BankAccount], "myAccount")
+  val person = system.actorOf(Props[Person], "billionaire")
+
+  person ! LiveTheLife(account)
+
+  /**
+   * Valid Questions!
+   * 1. Can we assume any ordering of messages?
+   * 2. Aren't we causing race conditions?
+   *
+   * To be figured out later!
+   * But now, let's go to sleep.
+   * - on April 2nd, 2022
+   * zzzzzz~
+   */
 }
